1058a7de44649ace22769e8330799f896dcd7de4
Raza <raza.khan@medsyshea>
2022-04-23 15:46:56 +0530

    - Adding CityWeatherListViewController

---
+++ WeatherApp/City Weather List View/CityWeatherListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CityWeatherListViewController.swift
+//  WeatherApp
+//
+//  Created by Raza on 23/04/22.
+//
+
+import UIKit
+
+class CityWeatherListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
